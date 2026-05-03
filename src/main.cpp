526fd657e6bd46729634498208a4c71a2124f604
Maxmomaric029 <cinthiab781@gmail.com>
2026-05-02 20:30:30 -0400

    Fix: Set working directory to app directory to improve asset loading

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "Game.h"
+#include "raylib.h"
 
 int main() {
+    // Asegurar que el directorio de trabajo es el de la aplicación
+    // Esto ayuda a encontrar los assets si se lanza desde otra carpeta
+    ChangeDirectory(GetApplicationDirectory());
+    
     Game soapDynasty;
     soapDynasty.Run();
     return 0;
